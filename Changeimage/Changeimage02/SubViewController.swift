a897b99a50871d8eabdb461f2c500274f08c864e
moritakun <hiro1028asaxkcm@yahoo.co.jp>
2019-10-04 15:05:53 +0900

    ボタンクリックと画面遷移はできた

---
+++ Changeimage/Changeimage02/SubViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SubViewController.swift
+//  Changeimage02
+//
+//  Created by 森田裕 on 2019/10/03.
+//  Copyright © 2019 森田裕. All rights reserved.
+//
+
+import Foundation
+
+class SubViewController:  UISubViewController {
+    
+}
---
+++ Changeimage/Changeimage02/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Changeimage02
+//
+//  Created by 森田裕 on 2019/10/02.
+//  Copyright © 2019 森田裕. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var label : UILabel!
+    @IBOutlet var button : UIButton!
+    @IBOutlet var countNum: UILabel!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    var count = 0
+    
+    
+    
+    //親呼び出し
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "test"
+        button.setTitle("Button", for:UIControl.State.normal)
+    }
+
+    
+    
+    
+    
+    @IBAction func ButtonClick(sender:Any){
+        count += 1
+        countNum.text = String(count)
+          if count%2 == 0 {
+              label.text = "偶数クリック"
+          }else{
+              label.text = "奇数クリック"
+        }
+        
+        //ボタンクリックで画面遷移
+         func NextBtn(_ sender: Any) {
+             super.viewDidLoad()
+         }
+    }
+//    @IBOutlet var ImageClick: UIImageView!
+//    @IBAction func ImageClick(sender:Any){
+//        ImageClick.image = #imageLiteral(resourceName: "black.png")
+//    }
+}
+
